4400740b4bcc1c934be55b63397677e3cb96c4d8
MiguelHenr <miguel.silva2702@hotmail.com>
2024-10-12 13:51:42 -0300

    Inicialização do projeto

---
+++ src/main/java/com/cefetmg/hgmanager/Controller/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.cefetmg.hgmanager.Controller;
+
+import com.cefetmg.hgmanager.View.HgManagerApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(HgManagerApplication.class);
+	}
+
+}
---
+++ src/test/java/com/cefetmg/hgmanager/HgManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cefetmg.hgmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HgManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
