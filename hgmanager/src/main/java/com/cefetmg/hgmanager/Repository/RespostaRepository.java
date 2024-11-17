66c619fb3765adb12c83dbcea9a4d7dd22721989
Júlia Gabrielly Pereira Barros <107952165+julia-sg@users.noreply.github.com>
2024-11-17 11:27:52 -0300

    Update RespostaRepository.java

--- hgmanager/src/main/java/com/cefetmg/hgmanager/Repository/RespostaRepository.java
+++ hgmanager/src/main/java/com/cefetmg/hgmanager/Repository/RespostaRepository.java
@@ -1,9 +1,17 @@
 package com.cefetmg.hgmanager.Repository;
 
+import com.cefetmg.hgmanager.Model.Reclamacao;
 import com.cefetmg.hgmanager.Model.Resposta;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RespostaRepository extends JpaRepository<Resposta, Long> {
+    @Query("SELECT r FROM Resposta r WHERE r.rc = :rc")
+    List<Resposta> findByReclamacao(@Param("rc") Reclamacao reclamacao);
 }
