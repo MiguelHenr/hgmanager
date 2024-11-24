e322d4cb1e9b82784fb676adb7e3dfd39cc9dbe3
alexbeldam <alexbeldam@gmail.com>
2024-11-24 13:34:07 -0300

    Login responsivo

--- hgmanager/src/main/resources/static/script/login.js
+++ hgmanager/src/main/resources/static/script/login.js
@@ -1,4 +1,8 @@
 let forms = document.getElementById('formulario');
+const inputs = document.querySelectorAll('input');
+const cpfInp = document.getElementById('usuario');
+const errorP = document.getElementById('error');
+const submitBtn = document.getElementById('entrar');
 
 async function validarLogin(event) {
 
@@ -11,7 +15,7 @@ async function validarLogin(event) {
     const data = Object.fromEntries(formData.entries());
 
     // Faz a requisição ao backend
-    const response = await fetch('http://localhost:8080/login', {
+    const response = await fetch('login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -20,9 +24,9 @@ async function validarLogin(event) {
     });
 
     if (!response.ok) {
-        const error = await response.text();
-        console.log(error);
-        alert('Erro: ' + error);
+        /*const error = await response.text();
+        console.log(error);*/
+        sinalize();
         return;
     }
 
@@ -33,7 +37,54 @@ async function validarLogin(event) {
     alert('Logado com sucesso! seja bem vindo: ' + result.nome);
 
     console.log(result);
+}
 
+function sinalize() {
+    if (errorP.classList.contains('hide')) {
+        errorP.classList.remove('hide');
+        submitBtn.classList.add('error');
+    }
+}
+
+function normalize() {
+    if (!errorP.classList.contains('hide')) {
+        errorP.classList.add('hide');
+        submitBtn.classList.remove('error');
+    }
+}
+
+function fixCpf() {
+    let cpf = cpfInp.value.replace(/\D/g, '').substring(0, 11);
+
+    if (cpf.length > 2) 
+        cpf = cpf.substring(0, 3) + '.' + cpf.substring(3);
+    if (cpf.length > 6) 
+        cpf = cpf.substring(0, 7) + '.' + cpf.substring(7);
+    if (cpf.length > 10) 
+        cpf = cpf.substring(0, 11) + '-' + cpf.substring(11);
+
+    cpfInp.value = cpf;
+}
+
+function reactCpf(e) {
+    if (e.key === 'Backspace') {
+        let index = cpfInp.selectionStart;
+        let cpf = cpfInp.value;
+
+        if (index > 0 && (cpf[index - 1] === '.' || cpf[index - 1] === '-')) {
+            cpf = cpf.substring(0, index - 2) + cpf.substring(index);
+            cpfInp.value = cpf;
+            cpfInp.setSelectionRange(index - 2, index - 2);
+            e.preventDefault();
+        }
+    }
 }
 
 forms.addEventListener('submit', (event) => validarLogin(event));
+
+for (let inp of inputs)
+    inp.addEventListener('focus', normalize);
+
+cpfInp.addEventListener('input', fixCpf);
+
+cpfInp.addEventListener('keydown', evt => reactCpf(evt));
