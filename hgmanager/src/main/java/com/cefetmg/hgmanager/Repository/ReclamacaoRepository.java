0c5778175a40935bf6483268bacf43acdab5f71b
alexbeldam <alexbeldam@gmail.com>
2024-11-25 11:36:28 -0300

    Caso de uso 3

--- hgmanager/src/main/java/com/cefetmg/hgmanager/Repository/ReclamacaoRepository.java
+++ hgmanager/src/main/java/com/cefetmg/hgmanager/Repository/ReclamacaoRepository.java
@@ -2,9 +2,16 @@ package com.cefetmg.hgmanager.Repository;
 
 import com.cefetmg.hgmanager.Model.Departamento;
 import com.cefetmg.hgmanager.Model.Reclamacao;
+import com.cefetmg.hgmanager.Model.Reserva;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReclamacaoRepository extends JpaRepository<Reclamacao, Long> {
+    @Query("SELECT r FROM Reclamacao r ORDER BY r.data DESC")
+    public List<Reclamacao> findAllOrdered();
 }
