7a59ca06a034cb2281457f03dff7eff2dc404ca8
alexbeldam <alexbeldam@gmail.com>
2024-11-26 09:08:12 -0300

    Caso de uso 2

--- hgmanager/src/main/resources/static/script/req.js
+++ hgmanager/src/main/resources/static/script/req.js
@@ -1,10 +1,40 @@
-const rejectBts = document.getElementsByClassName('reject');
-const approveBts = document.getElementsByClassName('approve');
+const ghost = document.getElementById('ghost');
+const main = document.getElementById('reqs');
+
+function loadPage() {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("POST", "requests", true);
+
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            main.innerHTML = xhr.responseText;
+
+            const rejectBts = document.getElementsByClassName('reject');
+            const approveBts = document.getElementsByClassName('approve');
+
+            for (let btn of approveBts)
+                btn.addEventListener("click", event => update(event, "approve"));
+        
+            for (let btn of rejectBts)
+                btn.addEventListener("click", event => update(event, "reject"));
+
+            ghost.classList.add('shoo');
+            main.classList.remove('shoo');
+        }
+    };
+
+    xhr.send();
+}
 
 function update(event, action) {
     const requestElement = event.target.closest("[data-index]");
     const id = requestElement.getAttribute("data-index");
     const dataToSend = JSON.stringify({ id: id, action: action });
+
+    disable(requestElement);
 
     const xhr = new XMLHttpRequest();
     xhr.open("POST", "/update-sol", true);
@@ -12,46 +42,51 @@ function update(event, action) {
 
     xhr.onload = function () {
         if (xhr.status >= 200 && xhr.status < 300) {
-            reloadRequestsContainer();
+            updateReqs(requestElement,action);
         } else {
+            enable(requestElement);
             console.error("Server error:", xhr.response);
         }
     };
 
     xhr.onerror = function () {
+        enable(requestElement);
         console.error("Request failed. Check your network connection.");
     };
 
     xhr.send(dataToSend);
 }
 
-function reloadRequestsContainer() {
-    const xhr = new XMLHttpRequest();
-    xhr.open("POST", "/requests", true);
+function updateReqs(el,action) {
+    const btnEl = el.querySelector('.btn');
+    const status = document.createElement('p');
 
-    xhr.onload = function () {
-        if (xhr.status >= 200 && xhr.status < 300) {
-            
-            document.getElementById("reqs").innerHTML = xhr.responseText;
-            attachEventListeners(); 
-        } else {
-            console.error("Failed to reload requests:", xhr.statusText);
-        }
-    };
+    status.innerHTML = action == 'approve' ? 'APROVADA' : 'REJEITADA';
 
-    xhr.onerror = function () {
-        console.error("Failed to reload requests. Check your network connection.");
-    };
-
-    xhr.send();
+    el.removeChild(btnEl);
+    el.appendChild(status);
 }
 
-function attachEventListeners() {
-    for (let btn of approveBts)
-        btn.addEventListener("click", event => update(event, "approve"));
+function disable(el) {
+    const btnEl = el.querySelector('.btn');
+    const btns = btnEl.querySelectorAll('button');
 
-    for (let btn of rejectBts)
-        btn.addEventListener("click", event => update(event, "reject"));
+    if (!btnEl.classList.contains('clicked'))
+        btnEl.classList.add('clicked');
+
+    for (let btn of btns)
+        btn.disabled = true;
 }
 
-attachEventListeners();
+function enable(el) {
+    const btnEl = el.querySelector('.btn');
+    const btns = btnEl.querySelectorAll('button');
+
+    if (btnEl.classList.contains('clicked'))
+        btnEl.classList.remove('clicked');
+
+    for (let btn of btns)
+        btn.disabled = false;
+}
+
+window.onload = loadPage;
