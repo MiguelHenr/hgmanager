5ad51c0c8b5c4b27ecf53872edb4f6fa76f8ea4b
alexbeldam <alexbeldam@gmail.com>
2024-12-11 14:54:04 -0300

    Inicio da paginação

--- hgmanager/src/main/java/com/cefetmg/hgmanager/Repository/ReservaRepository.java
+++ hgmanager/src/main/java/com/cefetmg/hgmanager/Repository/ReservaRepository.java
@@ -6,17 +6,18 @@ import com.cefetmg.hgmanager.Model.Usuario;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReservaRepository extends org.springframework.data.jpa.repository.JpaRepository<Reserva, Long> {
-    @Query("SELECT r FROM Reserva r ORDER BY r.inicio DESC")
-    List<Reserva> findAllOrdered();
+public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+    Page<Reserva> findAllByOrderByInicioDesc(Pageable pageable);
 
-    @Query("SELECT r FROM Reserva r WHERE r.professor = :p ORDER BY r.inicio DESC")
-    List<Reserva> findAllByProfessorOrdered(@Param("p") Usuario professor);
+    Page<Reserva> findAllByProfessorOrderByInicioDesc(Usuario professor, Pageable pageable);
 
     Reserva findReservaByRecurso(Recurso recurso);
 
